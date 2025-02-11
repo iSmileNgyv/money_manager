93c877afee89f38e9318ac426c2bbe2562344c34
ismile <ismayilnagiyev100@gmail.com>
2025-02-11 15:39:37 +0400

    v0.0.6

--- Presentation/MoneyManager.API/Program.cs
+++ Presentation/MoneyManager.API/Program.cs
@@ -111,7 +111,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors();
 app.UseRouting();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
